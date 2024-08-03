6f8da2c31014078d4c7af4b523624fc23864dca3
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-08-03 13:51:41 +0300

    Refactor Login Screen Imports
    Remove unused imports from the login screen to improve code readability and reduce clutter. This change specifically targets the removal of imports for custom authentication submit button, password reset prompt, quick login, and secondary login text widgets, which are no longer utilized in the login screen implementation.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ringo_media/widgets/custom_auth_submit_button.dart';
 import 'package:ringo_media/widgets/custom_login_screen_body.dart';
-import 'package:ringo_media/widgets/custom_password_reset_prompt.dart';
-import 'package:ringo_media/widgets/custom_quick_login.dart';
-import 'package:ringo_media/widgets/custom_register_prompt.dart';
-import 'package:ringo_media/widgets/custom_secondary_login_text.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -31,12 +26,3 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
